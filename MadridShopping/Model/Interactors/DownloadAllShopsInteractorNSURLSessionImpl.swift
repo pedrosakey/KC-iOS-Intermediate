d6f2e888b97c92e795a7abcb9b2bf8aafbe42c86
Pedro Sánchez Castro <pedro@sanchez.com>
2017-09-26 16:58:27 +0200

    DownloadAllShopsInteractorNSURLSession

---
+++ MadridShopping/Model/Interactors/DownloadAllShopsInteractorNSURLSessionImpl.swift
@@ -0,0 +1,41 @@
+//
+//  DownloadAllShopsInteractorNSURLSessionImpl.swift
+//  MadridShopping
+//
+//  Created by Pedro Sánchez Castro on 26/9/17.
+//  Copyright © 2017 pedrosapro. All rights reserved.
+//
+
+import Foundation
+
+class DownloadAllShopsInteractorNSURLSessionImpl: DownloadAllShopsInteractor {
+    func execute(onSuccess: @escaping (Shops) -> Void, onError: errorClosure) {
+        let urlString = "https://madrid-shops.com/json_new/getShops.php"
+        
+        let session = URLSession.shared
+        if let url = URL(string: urlString) {
+            let task = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+                
+                OperationQueue.main.addOperation {
+                    assert(Thread.current == Thread.main)
+                    
+                    if error == nil {
+                        let shops = parseShops(data: data!)
+                        onSuccess(shops)
+                    } else {
+                        if let myError = onError {
+                            myError(error!)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func execute(onSuccess: @escaping (Shops) -> Void) {
+        execute(onSuccess: onSuccess, onError: nil)
+    }
+    
+    
+}
---
+++ MadridShopping/Model/Interactors/JsonParser/JSONParser.swift
@@ -0,0 +1,32 @@
+//
+//  JSONParser.swift
+//  MadridShopping
+//
+//  Created by Pedro Sánchez Castro on 26/9/17.
+//  Copyright © 2017 pedrosapro. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+func parseShops(data: Data) -> Shops {
+    let shops = Shops()
+    do {
+        let jsonObject = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! Dictionary<String, Any>
+        let result = jsonObject["result"] as! [Dictionary<String, Any>]
+        
+        for shopJson in result {
+            let shop = Shop(name: shopJson["name"]! as! String)
+            shop.address = shopJson["address"]! as! String
+            shop.logo = shopJson["logo_img"] as! String
+            shop.image = shopJson["img"] as! String
+            shop.description = shopJson["description_en"] as! String
+            
+            shops.add(shop: shop)
+        }
+    } catch {
+        print("Error parsing JSON")
+    }
+    return shops
+}
